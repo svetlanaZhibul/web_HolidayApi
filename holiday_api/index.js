fd519b35bc0381b0a9bda6dbd21656139881dd62
svetlanaZhibul <3hibul@mail.ru>
2016-10-30 01:57:13 +0300

    Update index.js
    Simple xhr was added

--- holiday_api/index.js
+++ holiday_api/index.js
@@ -31,6 +31,8 @@ $(document).ready(function () {
       renderData();
       var myRequest = new Request(url + "&country=" + country + "&year=" + y + ((m) ? "&month="+m : "") + ((d) ? "&day="+d : ""));
       fetchfun(myRequest);
+	    
+     call_xhr();
     })
 
     var i = 0; 
@@ -57,10 +59,31 @@ $(document).ready(function () {
         }
     }
 
+    function call_xhr() {
+        var req;
+        if (window.XMLHttpRequest) {
+            req = new XMLHttpRequest();
+        }
+        if (req != null) {
+            var Url = "https://holidayapi.com/v1/holidays?key=883a4aca-792a-4530-b5a3-8a98c290fc05&country=US&year=2000&month=12";
+            document.getElementById("txt").innerHTML = "Request Sent...";
+            req.open("GET", Url, true);
+
+            req.onreadystatechange = function() {
+                if (req.readyState == 4 && req.status == 200) {
+                    document.getElementById("txt").innerHTML = "Response Received!";
+                }
+            }
+                
+            req.send();
+        } else {
+          window.alert("Error creating XmlHttpRequest object.");
+        }
+    } 
     // setInterval(renderData, 1000)
 
-    var fetchfun = function(req){
-        fetch(req)  
+    var fetchfun = function(Req){
+        fetch(Req)  
         .then(  
             function(response) {  
               if (response.status !== 200) {  
@@ -75,7 +98,7 @@ $(document).ready(function () {
             }  
         )  
         .catch(function(err) {  
-            console.log('Fetch Error :-S', err);  
+            console.log('Fetch Error', err);  
         });
     };
 
